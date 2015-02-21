8f5265bb348697937229f5785ba993907536c4c8
NickStrupat <nick.strupat@gmail.com>
2015-02-21 01:48:25 -0500

    initial code commit

---
+++ BoilerplateSource/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BoilerplateSource {
+	class Program {
+		const Int32 stackSize = 30000;
+		static readonly Byte[] stack = new Byte[stackSize];
+		const Int32 stackStartIndex = 0;
+		const Int32 stackEndIndex = stackSize - 1;
+		static Int32 stackIndex = stackStartIndex;
+		static Stack<Int64> jumps = new Stack<Int64>(); // instruction indexes
+
+		static void Main(string[] args) {
+			
+		}
+		static void GreaterThan() {
+			++stackIndex;
+			if (stackIndex == stackEndIndex)
+				stackIndex = stackStartIndex;
+		}
+		static void LessThan() {
+			if (stackIndex == stackStartIndex)
+				stackIndex = stackEndIndex;
+			--stackIndex;
+		}
+		static void Plus() {
+			++stack[stackIndex];
+		}
+		static void Minus() {
+			--stack[stackIndex];			
+		}
+		static void Dot() {
+			Console.Write((Char)stack[stackIndex]);
+		}
+		static void Comma() {
+			stack[stackIndex] = (Byte)Console.Read();
+		}
+		static void LeftBracket() {
+			if (stack[stackIndex] == 0) {
+				while (instructionStream.ReadByte() != ']')
+					;
+			}
+			else
+				jumps.Push(instructionStream.Position);
+		}
+		static void RightBracket() {
+			if (stack[stackIndex] != 0)
+				instructionStream.Seek(jumps.Peek(), SeekOrigin.Begin);
+			else
+				jumps.Pop();
+		}
+	}
+}
---
+++ BrainfuckSharpCompiler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace BrainfuckSharpCompiler {
+	class Program {
+		static void Main(String[] args) {
+			if (args.Length < 1)
+				throw new ArgumentException(String.Format("Usage: {0} source_file_path", AppDomain.CurrentDomain.FriendlyName));
+
+			var instructionStream = new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan);
+
+			var an = new AssemblyName { Name = args[0] };
+			var ad = AppDomain.CurrentDomain;
+			var ab = ad.DefineDynamicAssembly(an, AssemblyBuilderAccess.Save);
+			var mb = ab.DefineDynamicModule(an.Name, args[0] + ".exe");
+			var tb = mb.DefineType("Brainfuck.Program", TypeAttributes.Public | TypeAttributes.Class);
+			var fb = tb.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static, typeof(void), new Type[] { typeof(string[]) });
+			
+			// Emit the ubiquitous "Hello, World!" method, in IL
+			var ilg = fb.GetILGenerator();
+			ilg.EmitWriteLine("Hello, World!");
+			//ilg.Emit(OpCodes.Ldstr, "Hello, World!");
+			//ilg.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new [] { typeof(String) }));
+			ilg.Emit(OpCodes.Ret);
+			
+			// Seal the lid on this type
+			var t = tb.CreateType();
+			// Set the entrypoint (thereby declaring it an EXE)
+			ab.SetEntryPoint(fb, PEFileKinds.ConsoleApplication);
+			// Save it
+			ab.Save(args[0] + ".exe");
+		}
+	}
+}
