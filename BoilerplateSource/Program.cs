44c26ee2989c89dbfb1793b7dfc3d1e19e76f493
NickStrupat <nick.strupat@gmail.com>
2015-02-21 05:06:36 -0500

    more work

--- BoilerplateSource/Program.cs
+++ BoilerplateSource/Program.cs
@@ -6,38 +6,49 @@ using System.Text;
 
 namespace BoilerplateSource {
 	class Program {
-		const Int32 stackSize = 30000;
-		static readonly Byte[] stack = new Byte[stackSize];
-		const Int32 stackStartIndex = 0;
-		const Int32 stackEndIndex = stackSize - 1;
-		static Int32 stackIndex = stackStartIndex;
-		static Stack<Int64> jumps = new Stack<Int64>(); // instruction indexes
+		//static Stack<Int64> jumps = new Stack<Int64>(); // instruction indexes
 
 		static void Main(string[] args) {
-			
-		}
-		static void GreaterThan() {
+			const Int32 stackSize = 30000;
+			const Int32 stackStartIndex = 0;
+			const Int32 stackEndIndex = stackSize - 1;
+			var stack = new Byte[stackSize];
+			var stackIndex = stackStartIndex;
+
 			++stackIndex;
-			if (stackIndex == stackEndIndex)
-				stackIndex = stackStartIndex;
-		}
-		static void LessThan() {
-			if (stackIndex == stackStartIndex)
-				stackIndex = stackEndIndex;
+
 			--stackIndex;
-		}
-		static void Plus() {
+
 			++stack[stackIndex];
-		}
-		static void Minus() {
-			--stack[stackIndex];			
-		}
-		static void Dot() {
+
+			--stack[stackIndex];
+
 			Console.Write((Char)stack[stackIndex]);
-		}
-		static void Comma() {
+
 			stack[stackIndex] = (Byte)Console.Read();
 		}
+		//static void GreaterThan() {
+		//	++stackIndex;
+		//	//if (stackIndex == stackEndIndex)
+		//	//	stackIndex = stackStartIndex;
+		//}
+		//static void LessThan() {
+		//	//if (stackIndex == stackStartIndex)
+		//	//	stackIndex = stackEndIndex;
+		//	--stackIndex;
+		//}
+		//static void Plus() {
+		//	++stack[stackIndex];
+		//}
+		//static void Minus() {
+		//	--stack[stackIndex];			
+		//}
+		//static void Dot() {
+		//	Console.Write((Char)stack[stackIndex]);
+		//}
+		//static void Comma() {
+		//	stack[stackIndex] = (Byte)Console.Read();
+		//}
 		//static void LeftBracket() {
 		//	if (stack[stackIndex] == 0) {
 		//		while (instructionStream.ReadByte() != ']')
--- BrainfuckSharpCompiler/Program.cs
+++ BrainfuckSharpCompiler/Program.cs
@@ -5,6 +5,8 @@ using System.Reflection.Emit;
 
 namespace BrainfuckSharpCompiler {
 	class Program {
+		static readonly Type arrayElementType = typeof(Byte);
+		static readonly Type arrayType = typeof(Byte[]);
 		static void Main(String[] args) {
 			if (args.Length < 1)
 				throw new ArgumentException(String.Format("Usage: {0} source_file_path", AppDomain.CurrentDomain.FriendlyName));
@@ -16,15 +18,71 @@ namespace BrainfuckSharpCompiler {
 			var ab = ad.DefineDynamicAssembly(an, AssemblyBuilderAccess.Save);
 			var mb = ab.DefineDynamicModule(an.Name, args[0] + ".exe");
 			var tb = mb.DefineType("Brainfuck.Program", TypeAttributes.Public | TypeAttributes.Class);
-			var fb = tb.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static, typeof(void), new Type[] { typeof(string[]) });
+
+			var stackFieldBuilder = tb.DefineField("stack", arrayType, FieldAttributes.Static);
+			var stackIndexFieldBuilder = tb.DefineField("stackIndex", typeof (Int32), FieldAttributes.Static);
+
+			var incrementStackIndexMethodBuilder = tb.DefineMethod("IncrementStackIndex", MethodAttributes.Static, CallingConventions.Standard, typeof (void), null);
+			EmitIncrementStackIndexInstructions(incrementStackIndexMethodBuilder.GetILGenerator());
+
+			var decrementStackIndexMethodBuilder = tb.DefineMethod("DecrementStackIndex", MethodAttributes.Static, CallingConventions.Standard, typeof(void), null);
+			EmitDecrementStackIndexInstructions(decrementStackIndexMethodBuilder.GetILGenerator());
+
+			var fb = tb.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static, typeof(void), new [] { typeof(String[]) });
 			
-			// Emit the ubiquitous "Hello, World!" method, in IL
 			var ilg = fb.GetILGenerator();
-			ilg.EmitWriteLine("Hello, World!");
-			//ilg.Emit(OpCodes.Ldstr, "Hello, World!");
-			//ilg.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new [] { typeof(String) }));
-			//ilg.Emit(OpCodes.b);
-			//ilg.MarkLabel();
+
+			var buffer = ilg.DeclareLocal(arrayType);
+			var stackIndex = ilg.DeclareLocal(typeof (Int32));
+			
+			// initialize stack
+			ilg.Emit(OpCodes.Ldc_I4, 30000);
+			ilg.Emit(OpCodes.Newarr, arrayType);
+			ilg.Emit(OpCodes.Stloc_0);
+
+			// initialize stack index
+			ilg.Emit(OpCodes.Ldc_I4_0);
+			ilg.Emit(OpCodes.Stloc_1);
+
+			Int32 byteRead;
+			while ((byteRead = instructionStream.ReadByte()) != -1) {
+				var instruction = (Char) byteRead;
+				switch (instruction) {
+					case '>':
+						EmitIncrementStackIndexInstructions(ilg);
+						break;
+					case '<':
+						EmitDecrementStackIndexInstructions(ilg);
+						break;
+					case '+':
+						EmitIncrementByteOnStackInstructions(ilg);
+						break;
+					case '-':
+						EmitDecrementByteOnStackInstructions(ilg);
+						break;
+					case '.':
+						EmitWriteByteOnStackInstructions(ilg);
+						break;
+					case ',':
+						EmitReadByteOnStackInstructions(ilg);
+						break;
+					case '[':
+						//if (stack[stackIndex] == 0) {
+						//	while (instructionStream.ReadByte() != ']')
+						//		;
+						//}
+						//else
+						//	jumps.Push(instructionStream.Position);
+						break;
+					case ']':
+						//if (stack[stackIndex] != 0)
+						//	instructionStream.Seek(jumps.Peek(), SeekOrigin.Begin);
+						//else
+						//	jumps.Pop();
+						break;
+				}
+			}
+
 			ilg.Emit(OpCodes.Ret);
 			
 			// Seal the lid on this type
@@ -33,6 +91,54 @@ namespace BrainfuckSharpCompiler {
 			ab.SetEntryPoint(fb, PEFileKinds.ConsoleApplication);
 			// Save it
 			ab.Save(args[0] + ".exe");
+		}
+
+		static void EmitIncrementStackIndexInstructions(ILGenerator ilg) {
+			EmitStackIndexInstructions(ilg, OpCodes.Add);
+		}
+		static void EmitDecrementStackIndexInstructions(ILGenerator ilg) {
+			EmitStackIndexInstructions(ilg, OpCodes.Sub);
+		}
+		static void EmitStackIndexInstructions(ILGenerator ilg, OpCode addOrSub) {
+			ilg.Emit(OpCodes.Ldloc_0);
+			ilg.Emit(OpCodes.Ldc_I4_1);
+			ilg.Emit(addOrSub);
+			ilg.Emit(OpCodes.Stloc_1);
+		}
+
+		static void EmitIncrementByteOnStackInstructions(ILGenerator ilg) {
+			EmitByteOnStackInstructions(ilg, OpCodes.Add);
+		}
+		static void EmitDecrementByteOnStackInstructions(ILGenerator ilg) {
+			EmitByteOnStackInstructions(ilg, OpCodes.Sub);
+		}
+		static void EmitByteOnStackInstructions(ILGenerator ilg, OpCode addOrSub) {
+			ilg.Emit(OpCodes.Ldloc_0);
+			ilg.Emit(OpCodes.Ldloc_1);
+			ilg.Emit(OpCodes.Ldelema, arrayElementType);
+			ilg.Emit(OpCodes.Dup);
+			ilg.Emit(OpCodes.Ldobj, arrayElementType);
+			ilg.Emit(OpCodes.Ldc_I4_1);
+			ilg.Emit(addOrSub);
+			ilg.Emit(OpCodes.Conv_U1);
+			ilg.Emit(OpCodes.Stobj, arrayElementType);
+		}
+
+		static readonly MethodInfo ConsoleWriteChar = new Action<Char>(Console.Write).Method;
+		static void EmitWriteByteOnStackInstructions(ILGenerator ilg) {
+			ilg.Emit(OpCodes.Ldloc_0);
+			ilg.Emit(OpCodes.Ldloc_1);
+			ilg.Emit(OpCodes.Ldelem_U1);
+			ilg.Emit(OpCodes.Call, ConsoleWriteChar);
+		}
+
+		static readonly MethodInfo ConsoleRead = new Func<Int32>(Console.Read).Method;
+		static void EmitReadByteOnStackInstructions(ILGenerator ilg) {
+			ilg.Emit(OpCodes.Ldloc_0);
+			ilg.Emit(OpCodes.Ldloc_1);
+			ilg.Emit(OpCodes.Call, ConsoleRead);
+			ilg.Emit(OpCodes.Conv_U1);
+			ilg.Emit(OpCodes.Stelem_I1);
 		}
 	}
 }
